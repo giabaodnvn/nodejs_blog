4a7f1ef01d68dc750579936ef4b077f220237159
GiaBao <nguyenbao.nta@gmail.com>
2022-10-06 14:27:55 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('handlebars', exphbs({
+  extname: ".hbs"
+}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
